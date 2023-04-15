c191a435fc418d4ae53312073d231c873cdee430
Akshay-Kathiriya <akshay.k@solutelabs.com>
2023-04-15 14:41:36 +0530

    admin_side_loan_handler

--- controller/admin.js
+++ controller/admin.js
@@ -1,176 +1,131 @@
-const { validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs')
-const Admin = require('../models/admin')
-const jwt = require('jsonwebtoken')
-const path = require('path');
-require('dotenv').config();
-const Customer = require('../models/customer');
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const Admin = require("../models/admin");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+require("dotenv").config();
+const Customer= require("../models/customer");
 const secretKey = process.env.TOKEN_SECRET_KEY;
-const loan = require('../models/loan')
-
-// exports.signup = async (req, res, next) => {
-
-//     // const errors = validationResult(req);
-//     // if (!errors.isEmpty()) {
-//     //     const error = new Error('Validation failed');
-//     //     error.statusCode = 422,
-//     //         error.data = errors.array();
-//     //     throw error;
-//     // }
-//     const email = req.body.email;
-//     const name = req.body.name;
-//     const password = req.body.password;
-//     const maxLoanAmount = 1000000;
-//     try {
-//         const hashedPW = await bcrypt
-//             .hash(password, 12)
-
-//         const admin = new Admin({
-//             email: email,
-//             password: hashedPW,
-//             name: name,
-//             maxLoanAmount: maxLoanAmount
-//         });
-//         const result = await admin.save();
-
-//         res.status(201).json({
-//             message: "Admin created Sucessfully!",
-//             //  userId: result._id
-//         });
-//     } catch (err) {
-//         if (!err.statuCode) {
-//             err.statuCode = 500;
-//         }
-//         next(err); //now eeror will go to next error handling middleware
-//     };
-// }
+const loan = require("../models/loan");
+const Transaction= require("../models/customer");
+const ObjectId = require("mongodb").ObjectId;
 
 
-// exports.login = async (req, res, next) => {
-
-//     const email = req.body.email;
-//     const password = req.body.password;
-
-//     let loadUser;
-//     try {
-//         const user = await Admin.findOne({ email: email })
-
-//         if (!user) {
-//             const error = new Error('A User with email could not be found');
-//             error.statuCode = 401;
-//             throw error;
-//         }
-//         loadedUser = user;
-
-//         const isEqual = await bcrypt.compare(password, user.password);
-
-//         if (!isEqual) {
-//             const error = new Error('Wrong password');
-//             error.statusCode = 401;
-//             throw error;
-//         }
-//         //this will create an new signature and packs into a new jwt
-//         const token = jwt.sign({
-//             email: loadedUser.email,
-//             userId: loadedUser._id.toString()
-//         }, secretKey,
-//             { expiresIn: '1h' }
-//         );
-//         res.status(200)
-//             .json({
-//                 token: token,
-//                 userId: loadedUser._id.toString()
-//             })
-//     }
-//     catch (err) {
-//         if (!err.statuCode) {
-//             err.statuCode = 500;
-//         }
-//         console.log(err)
-//         next(err);//now eeror will go to next error handling middleware
-
-//     }
-
-// }
-
-exports.getCustomerDetails= async(req, res)=>{
-
-   try{
+exports.getCustomerDetails = async (req, res) => {
+  try {
     //we can add below code separately using middleware authenticationtoken
     // const token = req.body.token;
-    // const adminDetails = jwt.verify(token, secretKey); 
+    // const adminDetails = jwt.verify(token, secretKey);
     const details = await Customer.find();
     res.json(details);
-   }
-   catch(err){
+  } catch (err) {
     console.error(err);
-    res.status(500).send('Server Error');
-   }
-}
+    res.status(500).send("Server Error");
+  }
+};
 
-
-exports.totalAmount = async(req, res)=>{
-    try{
-        let total = 0;
-        const details = await Customer.find();
-        // const items = JSON.stringify(details)
-        console.log(details[0].balance)
+exports.totalAmount = async (req, res) => {
+  try {
+    let total = 0;
+    const details = await Customer.find();
+    // const items = JSON.stringify(details)
+    console.log(details[0].balance);
     //    details.reduce(item=>{
     //         console.log(typeof item.balance)
     //         total+=item.balance;
     //     })
 
-        for(let i=0; i<details.length; i++){
-            total+=details[i].balance
-        }
-        res.status(200).send({total});
-    }catch(err){
-        console.log(err);
-        res.status(500).send('Server Error');
+    for (let i = 0; i < details.length; i++) {
+      total += details[i].balance;
     }
-}
+    res.status(200).send({ total });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
+};
 
-exports.loanrequest = async (req, res)=>{
-    try{
-        const loanDetails = await loan.find();
-        res.status(200).send(loanDetails);
-        
+exports.loanrequest = async (req, res) => {
+  try {
+    const loanDetails = await loan.find();
+    res.status(200).send(loanDetails);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
+};
 
-    }catch(err){
-        console.log(err);
-        res.status(500).send('Server Error');
-    }
-}
-
-exports.isapprove = async (req, res)=>{
-    try{
+exports.isapprove = async (req, res) => {
+  //loanrequesthandler
+  try {
     const customerId = req.body.id;
     const isapproved = req.body.status;
     // ["pending", "approved", "rejected"]
     const newDetails = {
-        customer: customerId,
-        status: isapproved
-    }
+      status: isapproved,
+    };
+    // console.log("ID : ", typeof customerId);
+    //approved
+    if(isapproved==='approved'){
+    //reflet loan amount in customer schema
 
-    let updateLoanDetails = await loan.updateOne({"customer":customerId},{$set: newDetails});
+    const id = new ObjectId(customerId).toString();
+    let customerDetails = await Customer.findById({ _id: id });
+    // console.log(customerDetails);
+    const loanAmount = await loan.findOne({ customer : id })
+    // console.log(loanAmount);
+    let balance = customerDetails.balance
+    balance+=loanAmount.amount;
+    // console.log(balance)
+
+    await Customer.updateOne({_id:id},{ $set:{balance} })
+    // let customerDetails1 = await Customer.findById({ _id: id });
+    // console.log(customerDetails1);
+   
+    //update transactions
+    const transaction=new Transaction({
+        type:'Loan',
+        senderAccountNumber: 0,
+        receiverAccountNumber: customerDetails.accountNumber,
+        amount:[{
+            debitAmount:loanAmount,
+        }],
+        date:Date.now()
+    })
     
-    if(!updateLoanDetails){
-        res.send("Failed")
+    await transaction.save();
+    console.log("after transaction")
+    //reject
+    //
+    let updateLoanDetails;
+    if (isapproved === "approved") {
+      updateLoanDetails = await loan.updateOne(
+        { customer: customerId },
+        { $set: newDetails }
+      );
     }
-    res.send("Updated.")
+     else {
+    }
+    if (!updateLoanDetails) {
+      res.send("Failed");
+    }
+    }else{
 
-    
-    }catch(err){
-        console.log(err);
-        res.status(500).send('Server Error');
     }
 
-}
 
-//loadaproval name, email,loanamount, balance, 
+
+    res.send("Updated.");
+  
+    } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
+};
+
+//loadaproval name, email,loanamount, balance,
 // http://local.../loanrequst
 // name,email,loanamount
 
 // http://lacal.../loanresponse
 // reject or approved
-
--- models/customer.js
+++ models/customer.js
@@ -32,11 +32,21 @@ const customerSchema = new mongoose.Schema({
         required: true,
         default: 0,
     },
-    transactions: [{
-        type: { type: String, required: true },
-        amount: { type: Number, required: true },
-        date: { type: Date, required: true, default: Date.now },
-    }, ],
+   
+});
+const transactionSchema = new mongoose.Schema({
+    type: { type: String, required: true },
+    senderAccountNumber: { type: Number, required: true },
+    receiverAccountNumber: { type: Number, required: true },
+    amount: [{
+        debitAmount: { type: Number, required: true, default: 0 },
+        creditAmount: { type: Number, required: true, default: 0 },
+    }],
+    date: { type: Date, required: true, default: Date.now }
 });
 
+// module.exports = mongoose.model("Customer", customerSchema);
+// module.exports = mongoose.model("Transaction", transactionSchema);
+
 module.exports = mongoose.model("Customer", customerSchema);
+module.exports.Transaction = mongoose.model("Transaction", transactionSchema);
